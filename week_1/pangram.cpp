f7d0229a67a6cad7595d28626608beb1a4ff77ef
TheTeaCup <lw0482352@gmail.com>
2023-05-02 16:16:46 -0400

    add week one

---
+++ week_1/pangram.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout<<"Pangram\n\n";
+    cout<<"The quick\nbrown fox\n";
+    cout<<"\njumps\n\n";
+    cout<<"over the\nlazy dog.\n";
+}
