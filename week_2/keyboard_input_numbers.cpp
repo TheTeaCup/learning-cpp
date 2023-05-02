91189057ad32d751506717d62c16fe73220cab44
TheTeaCup <lw0482352@gmail.com>
2023-05-02 16:15:45 -0400

    add week two

---
+++ week_2/keyboard_input_numbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int num1, num2, answer;
+
+    cout << "Enter an integer value: ";
+    cin >> num1;
+
+    cout << "Enter a second integer value: ";
+    cin >> num2;
+
+    answer=num1+num2;
+
+    cout << "The sum of " << num1 << " and " << num2 << " is " << answer;
+
+}
---
+++ week_2/output_formatting.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x=100;
+    int y=250;
+    int z=x+y;
+
+    cout<<"Sum of x+y = " << z;
+
+    cout << "\nSum of " << x << " + " << y << " = " << z;
+}
