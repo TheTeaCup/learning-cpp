ded217aea0e70e7c3a69645dd5b9f58c5d43420c
TheTeaCup <lw0482352@gmail.com>
2023-05-03 00:48:41 -0400

    final assignment done

--- week_8/arrays.cpp
+++ week_8/arrays.cpp
@@ -1,41 +1,30 @@
 #include <iostream>
-/* this is not done yet!*/
+#include <cmath>
+
 using namespace std;
-int A[3][3];
-int findAverage(int course ,int noOfGrades ){
 
-    float sum,average;
-
-
-    for (int j = 0; j<noOfGrades; j++) {
-
-        sum += A[course][j];
-
-    }
-    average = sum / noOfGrades;
-    return average;
- }
 
 int main() {
     
-    int gradebook[3][3] = {
-        {80, 95, 65},
-        {99, 75, 80},
-        {65, 80, 90}
-    };
+  int gradebook[3][3] = {
+      {80, 95, 65},
+      {99, 75, 80},
+      {65, 80, 90}
+  };
     
-    cout << "Gr1 Gr2 Gr3 Avg" << endl;
-    
-    int i,j;
-	
-	
-	for(i=0;i<3;i++)
-	{
-		for(j=0;j<3;j++)
-		{
-			cout<< gradebook[i][j] << "  ";
-		}
-		cout<< endl;
-	}
-    
+  cout << "Gr1 Gr2 Gr3 Avg" << endl;
+
+  double sum = 0;
+  double average[3];
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      sum = sum + gradebook[i][j];
+      cout << gradebook[i][j] << "  ";
+
+    }
+    average[i] = sum / 3;
+    sum = 0;
+    cout << round(average[i]) << endl;
+
+  }
 }
